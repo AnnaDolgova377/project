22fbe3dac111ad602ccb378c457b3994da256903
Anna <anjuta377@gmail.com>
2022-05-24 18:24:44 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+console.log('arr' + ' - objekt');
+
+let incr = 10,
+	decr = 10;
+
+	// incr++;
+	// decr--;
+
+	console.log(incr++);
+	console.log(decr);
+	console.log(decr);
+
+	console.log(2 + 2 * 2 !== '6');
