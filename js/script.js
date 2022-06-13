2e3efdb76d3d7fccf494a4a81975dfc93ba0ab2b
Anna <anjuta377@gmail.com>
2022-06-13 17:29:44 +0300

    Условия и циклы

--- js/script.js
+++ js/script.js
@@ -8,12 +8,71 @@ const personalMovieDB = {
 	privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-	b = prompt('На сколько оцените его?', ''),
-	c = prompt('Один из последних просмотренных фильмов?', ''),
-	d = prompt('На сколько оцените его?', '');
+for(let i=0; i<2; i++){
+	const a = prompt('Один из последних просмотренных фильмов?', ''),
+		b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+	if(a != null && b != null && a !='' && b !='' && a.length < 50){
+		personalMovieDB.movies[a] = b;
+		console.log('done');
+	} else {
+		i--;
+		console.log('error');
+	}
+};
+
+
+// let i=0;
+// do {
+// 	const a = prompt('Один из последних просмотренных фильмов?', ''),
+// 		b = prompt('На сколько оцените его?', '');
+
+// 	if(a != null && b != null && a !='' && b !='' && a.length < 50){
+// 		personalMovieDB.movies[a] = b;
+// 		console.log('done');
+// 	i++
+// 	}
+// }
+// while (i<2);
+
+// let i=0;
+// while(i<2){
+// 	const a = prompt('Один из последних просмотренных фильмов?', ''),
+// 		b = prompt('На сколько оцените его?', '');
+
+// 	if(a != null && b != null && a !='' && b !='' && a.length < 50){
+// 		personalMovieDB.movies[a] = b;
+// 		console.log('done');
+// 		i++;
+// 	};
+// };
+
+if (personalMovieDB.count < 10) {
+	console.log("Просмотрено довольно мало фильмов");
+} else if(personalMovieDB.count >=10 && personalMovieDB.count < 30) {
+	console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+	console.log("Вы киноман");
+} else {
+	console.log("Вы киноман");
+}
 
 console.log(personalMovieDB);
+
+
+
+// switch(num){
+// 	case 49:
+// 		console.log('Неверно');
+// 	break;
+// 	case 100:
+// 		console.log('Неверно');
+// 	break;
+// 	case 50:
+// 		console.log('В точку!');
+// 	break;
+// 	default:
+// 		console.log('Не в этот раз!');
+// 	break;
+// };
+
